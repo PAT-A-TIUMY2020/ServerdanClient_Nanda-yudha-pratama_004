0d6d711ba58b847137113a0b9d36af1c19f67563
Nanda yudha pratama <nanda.yudha.ft18@mail.umy.ac.id>
2020-10-31 15:10:36 +0700

    Add project files.

---
+++ Server_Nanda yudha pratama_004/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ServiceModel;
+using ServiceMtk_P1_20180140004;
+
+namespace Server_Nanda_yudha_pratama_004
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Enabled = true;
+            button2.Enabled = false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = true;
+            label3.Text = "Klik OFF Untuk Mematikan Server";
+            label2.Text = "Server ON";
+
+            Server server = new Server();
+            server.OnServer();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Enabled = false;
+            label3.Text = "Klik ON Untuk Menjalankan Server";
+            label2.Text = "Server OFF";
+
+            Server server = new Server();
+            server.OffServer();
+        }
+    }
+}
---
+++ Server_Nanda yudha pratama_004/Server.cs
@@ -0,0 +1,47 @@
+﻿using ServiceMtk_P1_20180140004;
+using System;
+using System.ServiceModel;
+
+namespace Server_Nanda_yudha_pratama_004
+{
+    internal class Server
+    {
+        internal void OnServer()
+        {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(Matematika));
+                hostObj.Open();
+                Console.WriteLine("Server is Ready!!!");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+
+        internal void OffServer()
+        {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(Matematika));
+                hostObj.Open();
+                Console.WriteLine("Server is Off!!!");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
